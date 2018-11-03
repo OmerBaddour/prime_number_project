bb0282dbc72deb731dc232466e8a6a41f67a93fc
Omer <ob2267@columbia.edu>
2018-11-03 17:49:24 -0400

    Problem2 part 1.

--- Problem2_Sieve.py
+++ Problem2_Sieve.py
@@ -4,12 +4,6 @@
 Part 1.
 Generating all primes up to and including n
 
-Populate a set of numbers p from 2 to n incrementing by 1
-for each number y < math.floor(sqrt(n))
-set y = numbers 1 to n incrementing by y
-p = p - y
-return p
-
 """
 
 import math
@@ -20,14 +14,15 @@ def sieve(n):
 
 	#creating a list from 2 to n
 	for x in range(0,n-2):
-		primes[x] = x+2
+		primes.append(x+2)
 
-	for x in range(2,math.floor(math.sqrt(n))):
-		for y in range(2,math.floor(n/y)):
-			primes.remove(y*x) 
+	#for all numbers from 2 to floor(sqrt(n)), remove all multiples of each number (y) from 2 to floor(n/y)
+	for x in range(2,math.floor(math.sqrt(n))+1):
+		for y in range(2,math.floor(n/x)+1):
+			if (y*x) in primes:
+				primes.remove(y*x) 
 
 	return primes
 
 if __name__ == '__main__':
 	print(sieve(100))
-	
