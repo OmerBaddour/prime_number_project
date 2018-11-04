44f896b9955f236bbc480be7c5b52e8aa40ce904
Omer <ob2267@columbia.edu>
2018-11-04 16:49:32 -0500

    Problem 3. parts 1. 2. and 3. so JUICY

--- Problem3_Primality_Testing.py
+++ Problem3_Primality_Testing.py
@@ -2,18 +2,12 @@
 3. Primality Testing
 
 Part 1.
-Why would I do without Sieve boi?
+Check if a number is prime using Trial Division.
 
 """
+import math 
 
-"""
-Part 2.
-Assuming between is inclusive of sqrt(n)
-
-"""
-
-import math
-
+#can delete if scope of problem 2 is sufficient
 def sieve(n):
 
 	primes = []
@@ -29,19 +23,114 @@ def sieve(n):
 				primes.remove(y*x) 
 
 	return primes
+#end potential deletion
+
+def prime_test_trial(n):
+
+	primes = sieve(math.floor(math.sqrt(n)))
+	solutions = []
+	proceed = True
+	#track position
+	i = 0
+	while(proceed):
+		#iterate through list of primes from 2 to sqrt(n), check for divisibility
+		if(primes[i] % n == 0):
+			solutions.append(primes[i])
+		i += 1
+
+	if(len(solutions) > 0):
+		return(str(n) + " is not prime.")
+	else:
+		return(str(n) + " is prime.")
+
+"""
+Part 2.
+Check if a number is prime using the Sieve of Eratosthenes.
+
+"""
+
+import math
+
+#can delete if scope of problem 2 is sufficient
+def sieve(n):
+
+	primes = []
+
+	#creating a list from 2 to n
+	for x in range(0,n-1):
+		primes.append(x+2)
+
+	#for all numbers from 2 to floor(sqrt(n)), remove all multiples of each number (y) from 2 to floor(n/y)
+	for x in range(2,math.floor(math.sqrt(n))+1):
+		for y in range(2,math.floor(n/x)+1):
+			if (y*x) in primes:
+				primes.remove(y*x) 
+
+	return primes
+#end potential deletion
 
 def prime_test_sieve(n):
 
 	primes = sieve(n)
-	solutions = []
-	for x in range(2,math.floor(math.sqrt(n))+1):
-		if((n % x == 0) and (x in primes)):
-			solutions.append(x)
-
-	if(len(solutions) > 0):
-		return(str(solutions) + " = The set of prime(s) between 2 and sqrt(" + str(n) + ") which divide " + str(n))
+	#if n is in sieve(n), we know n is prime
+	#only need to check last value of output of sieve(n), as sieve(n) only returns primes less than or equal to n
+	if(primes[len(primes)-1] == n):
+		return(str(n) + " is prime.")
 	else:
-		return("There are no primes between 2 and sqrt(" + str(n) + ") which divide " + str(n))
+		return(str(n) + " is not prime.")
+
+
+"""
+Part 3. 
+Check if a number is prime using Fermat's Little Theorem.
+
+"""
+
+import random
+
+def prime_test_fermat(p):
+
+	prime = True
+	#in case we get a Fermat liar, repeat 3 times
+	#now very unlikely to yield a false positive
+	repeats = 0
+
+	while(prime and repeats < 3):
+		result = 1
+		#random choice of alpha
+		alpha = random.randint(2,p-1)
+		#array to store values of repeated squares
+		rept_sqrs = [alpha]
+		exp = 2
+
+		#populate array containing repeated squares of alpha (mod p)
+		while(exp < p):
+			rept_sqrs.append(rept_sqrs[len(rept_sqrs)-1]**2 % p)
+			exp *= 2
+
+		#calculate alpha^(p-1) (result)
+		#want to factor alpha^(p-1) using repeated squares of alpha
+		temp = p-1
+		index = len(rept_sqrs) - 1
+		while(index >= 0):
+			if(temp >= 2**index):
+				result *= rept_sqrs[index]
+				temp -= 2**index
+			else:
+				index -= 1
+
+		#check if alpha^(p-1) is congruent to 1 (mod p)
+		if(result % p != 1):
+			prime = False
+		repeats += 1;
+
+	if(not prime):
+		return(str(p) + " is not prime.")
+	else:
+		return(str(p) + " is prime.")
 
 if __name__ == '__main__':
-	print(prime_test_sieve(30))
+	#tests
+	print(prime_test_trial(101))
+	print(prime_test_sieve(101))
+	print(prime_test_fermat(101))
